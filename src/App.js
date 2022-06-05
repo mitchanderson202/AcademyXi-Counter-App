8c9b52a62ebf08665f0c052b7b0842c95af515fe
mitch <mitchellanderson202@hotmail.com>
2022-06-05 11:31:59 +1000

    completed assignment

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const Increment = () => {
+    setCount(count + 1);
+  }
+
+  const Decrement = () => {
+    setCount(count - 1);
+  }
+
+  const Reset = () => {
+    setCount(0);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <button onClick={Increment}>+</button>
+    <h1>{count}</h1>
+    <button onClick={Decrement}>-</button>
+    <button onClick={Reset}>Reset</button>
     </div>
-  );
+    )
 }
 
 export default App;
